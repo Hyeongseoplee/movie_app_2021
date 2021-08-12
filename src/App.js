fe2eda08b6353a8ab9059ce3d9eb5c048e0bb47a
vennylee <hyungsup42134@gmail.com>
2021-08-12 12:41:42 +0900

    About component Life cycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 class App extends React.Component {
-
+  constructor(props){
+super(props);
+console.log('hello!')
+  }
   state = {
     number : 0
   }
@@ -14,7 +17,20 @@ class App extends React.Component {
     this.setState(current => ({ number : current.number - 1}) );
   }
 
+  componentDidMount() {
+    console.log('it was rendered at the first time')
+  }
+
+  componentDidUpdate() {
+    console.log('i just updated!');
+  }
+
+  componentWillUnmount() {
+    console.log('good bye, cruel world')
+  }
+
   render() {
+    console.log("i'm rendering");
     return (
       <div>
       <h1>the number is {this.state.number}</h1>
